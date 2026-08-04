5e3af1ae3e38920606c5d544ea86985e06ab210a
Jay <jaypranil1503@gmail.com>
2026-08-05 02:22:27 +0530

    implementes certain utils to be used and error handling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+import dotenv from "dotenv"
 import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
-import connectDB from "./db";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
 
+
+dotenv.config({
+    path : "./env"
+})
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+        
+    })
+})
+.catch((err) => {
+    console.log("Mongo db connection failed!! ",err);
+})
+
+
+
 
 /*
 
