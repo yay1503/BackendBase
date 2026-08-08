5606ff7fd9f9ed2916340ca5dfbd7e60e026b738
Jay <jaypranil1503@gmail.com>
2026-08-08 20:32:54 +0530

    added some routes and chnaged the DNS as the app was crashing

--- src/app.js
+++ src/app.js
@@ -2,17 +2,24 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
-const express = require('express');
 const app = express()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials = true
+    credentials: true
 }))
 
 app.use(express.json({limit : "16kb"}))
 app.use(express.urlencoded({extended: true , limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+//routes
+
+import userRouter from "./routes/user.routes.js"
+
+
+app.use("/api/v1/users",userRouter)
 
 export {app}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+import dns from 'dns';
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+
 import dotenv from "dotenv"
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 
 dotenv.config({
